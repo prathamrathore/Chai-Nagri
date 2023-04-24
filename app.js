6a14b10b514a19adc62d7a6f88f2e724ecf92dc3
prathamrathore <pk819199@gmail.com>
2023-04-24 16:33:21 +0530

    added client side validation in forms

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Chainagri = require('./models/chainagri');
@@ -38,33 +40,43 @@ app.get('/chainagri/new', (req, res) => {
     res.render('chainagri/new');
 })
 
-app.post('/chainagri', async (req, res) => {
+app.post('/chainagri', catchAsync(async (req, res) => {
     const chainagri = new Chainagri(req.body.chainagri);
     await chainagri.save();
     console.log(req.body)
     res.redirect(`/chainagri/${chainagri._id}`)
-})
+}))
 
-app.get('/chainagri/:id', async (req, res,) => {
+app.get('/chainagri/:id', catchAsync(async (req, res,) => {
     const chainagri = await Chainagri.findById(req.params.id)
     res.render('chainagri/show', { chainagri });
-});
+}));
 
-app.get('/chainagri/:id/edit', async (req, res) => {
+app.get('/chainagri/:id/edit', catchAsync(async (req, res) => {
     const chainagri = await Chainagri.findById(req.params.id)
     res.render('chainagri/edit', { chainagri });
-})
+}))
 
-app.put('/chainagri/:id', async (req, res) => {
+app.put('/chainagri/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const chainagri = await Chainagri.findByIdAndUpdate(id, { ...req.body.chainagri });
     res.redirect(`/chainagri/${chainagri._id}`)
-});
+}));
 
-app.delete('/chainagri/:id', async (req, res) => {
+app.delete('/chainagri/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Chainagri.findByIdAndDelete(id);
     res.redirect('/chainagri');
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 
