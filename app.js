0fe66a505c23cd10f7128428ec890a5cf58d103d
prathamrathore <pk819199@gmail.com>
2023-04-27 01:29:06 +0530

    Added final bootstrap and handled security issues(sanitization etc...)

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-
 const reviewsRoutes = require('./routes/reviews');
 const chainagriRoutes = require('./routes/chainagri');
 const userRoutes = require('./routes/users');
+const mongoSanitize = require('express-mongo-sanitize');
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect('mongodb://localhost:27017/chai-nagri', {
     useNewUrlParser: true,
@@ -38,6 +39,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
