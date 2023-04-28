189b904098d35da3edbcb0a5fa6aef73b1212055
prathamrathore <pk819199@gmail.com>
2023-04-28 22:27:32 +0530

    changed database

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const MongoDBStore = require("connect-mongo")(session);
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/chai-nagri';
 // 'mongodb://localhost:27017/chai-nagri'
-mongoose.connect(dbUrl, {
+mongoose.connect('mongodb://localhost:27017/chai-nagri', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
