51f7b70d0fe60abdc98fcfb77fe61bc85797055e
prathamrathore <pk819199@gmail.com>
2023-05-15 18:03:26 +0530

    updated app.js

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 // const dbUrl = process.env.DB_URL;
 const MongoDBStore = require("connect-mongo")(session);
 
-let dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/chai-nagri';
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/chai-nagri';
+const dbUrl = 'mongodb://localhost:27017/chai-nagri';
 
 if(process.env.docker !== undefined && process.env.docker === 'true')
     dbUrl = 'mongodb://mongo:27018/chai-nagri'
