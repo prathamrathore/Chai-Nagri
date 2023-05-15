dbae5bb3405094b715686d3477ea6c4caf1f844f
prathamrathore <pk819199@gmail.com>
2023-05-15 12:11:50 +0530

    checking

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const MongoDBStore = require("connect-mongo")(session);
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/chai-nagri';
 // 'mongodb://localhost:27017/chai-nagri'
 //'mongodb://mongo:27017/chai-nagri'
-mongoose.connect('mongodb://localhost:27017/chai-nagri', {
+mongoose.connect('mongodb://mongo:27017/chai-nagri', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
